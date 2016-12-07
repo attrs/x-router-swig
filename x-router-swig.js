63a7472e061476551fcbcad3f20f1d9bd59512f5
joje <joje.attrs@gmail.com>
2016-12-08 03:04:25 +0900

    update x-router@0.4

--- x-router-swig.js
+++ x-router-swig.js
@@ -3,26 +3,27 @@ var swig = require('swig');
 var compiled = {};
 module.exports = (function(defaults) {
   defaults = defaults || {};
-  return function(src, options, done) {
-    if( !src ) return done(new Error('missing src'));
-    if( typeof src !== 'string' ) return done(new TypeError('src must be a string(url)'));
-    
+  
+  return function(options, done) {
     var target = options.target;
+    var src = options.src;
+    var html = options.html;
     var cache = options.cache === true ? true : (defaults.cache === true ? true : false);
     
-    var render = function(tpl, locals) {
-      var html = tpl(locals);
-      target.innerHTML = html;
+    var render = function(err, html) {
+      if( err ) return done(err);
+      if( typeof html === 'object' && html.html ) html = html.html;
+      if( typeof html !== 'string' ) return done(new Error('html must be a string'));
+      
+      var tpl = compiled[src] || swig.compile(html, defaults.options);
+      if( src ) compiled[src] = tpl;
+      
+      target.innerHTML = tpl(options.locals || {});
       done();
     };
     
-    if( cache && compiled[src] ) render(compiled[src], options.locals || {});
-    else this.util.ajax(src, function(err, html) {
-      if( err ) return done(err);
-      
-      var template = swig.compile(html, defaults.options);
-      if( cache ) compiled[src] = template;
-      render(template, options.locals || {});
-    });
+    if( src ) this.util.ajax(src, render);
+    else if( html ) render(null, html);
+    else done(new Error('missing src or html'));
   };
 });
